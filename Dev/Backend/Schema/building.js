76feae6b095458577a150ca2dd36bd5777e753b6
jukle <julien.kle02@gmail.com>
2024-11-22 17:01:29 +0100

    Fixing connection issues

--- Dev/Backend/Schema/building.js
+++ Dev/Backend/Schema/building.js
@@ -1,22 +1,10 @@
 const mongoose = require('mongoose');
 
 const buildingSchema = new mongoose.Schema({
-    buildingId: {
-        type: String,
-        required: true,
-    },
-    counter: {
-        type: Number,
-        required: true,
-    },
-    cameraId: {
-        type: String,
-        required: true,
-    },
-    lastUpdated: {
-        type: Date,
-        required: true,
-    }
+    buildingId: { type: String, required: true },
+    counter: { type: Number, required: true },
+    cameraId: { type: String, required: true },
+    lastUpdated: { type: Date, required: true },
 });
 
 const Building = mongoose.model('Building', buildingSchema);
