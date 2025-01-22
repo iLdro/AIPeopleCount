de6c8775f12b1d2f52858cd6fb64a892272e5c5b
Julien Klein <44435573+iLdro@users.noreply.github.com>
2025-01-22 16:03:51 +0100

    Iniating front + fixing back

--- Dev/Backend/Database/db.js
+++ Dev/Backend/Database/db.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 async function connectDB() {
     const uri = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@aipeoplecount.rdtpe.mongodb.net/AIPeopleCount?retryWrites=true&w=majority`;
-
     try {
         await mongoose.connect(uri); // No additional options needed
         console.log("Connected to MongoAtlas");
--- Dev/Backend/Schema/building.js
+++ Dev/Backend/Schema/building.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const buildingSchema = new mongoose.Schema({
-    buildingId: { type: String, required: true },
+    buildingId: { type: String, required: true,},
     counter: { type: Number, required: true },
     cameraId: { type: String, required: true },
     lastUpdated: { type: Date, required: true },
