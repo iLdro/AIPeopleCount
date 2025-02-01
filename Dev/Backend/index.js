505d788b1e82bafde840de3924963580d28efd7d
Valentin L <valentin.labrune@gmail.com>
2025-02-01 01:45:58 +0100

    people in building

--- Dev/Backend/index.js
+++ Dev/Backend/index.js
@@ -13,7 +13,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
-app.use(cors())
 
 // Route
 app.get("/", (req, res) => {
