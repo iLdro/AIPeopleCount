389c0f890135d6949633af56e459922c73586536
Julien Klein <44435573+iLdro@users.noreply.github.com>
2025-02-04 10:46:57 +0100

    Bug

--- Dev/Backend/Service/cameraService.js
+++ Dev/Backend/Service/cameraService.js
@@ -1,12 +1,14 @@
 const { Camera } = require('../Schema/camera');
 
 async function CameraUpdate(cameraId, people) {
+    console.log('CameraUpdate', cameraId, people);
     let camera = new Camera({ cameraId: cameraId, actualOnCamera: people, lastUpdated: new Date() });
     return await camera.save();
 }
 
 async function getCameraPeople(cameraId) {
-    let camera = await Camera.findOne({ cameraId: cameraId });
+    let camera = await Camera.findOne({ cameraId: cameraId }, { _id: 0, __v: 0 }).sort({ lastUpdated: -1 });
+    console.log('getCameraPeople', camera); // This line is not needed
     return camera ? camera.actualOnCamera : null;
 }
 
